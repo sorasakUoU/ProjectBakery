d97a0971888a7fc5a9e416878bc4ad779c3dbb01
sorasakUoU <nightontherock@gmail.com>
2016-03-09 08:57:48 +0700

    CheckSpace

--- app/src/main/java/kawinpart/sorasak/buybakery/SignUpActivity.java
+++ app/src/main/java/kawinpart/sorasak/buybakery/SignUpActivity.java
@@ -2,12 +2,57 @@ package kawinpart.sorasak.buybakery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText, IdCardEditText;
+    private String nameString, userString, passwordString, IdCardString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
+
+
+    } //Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+        IdCardString = IdCardEditText.getText().toString().trim();
+
+        // Check Space
+        if (checkspace()) {
+
+        } else {
+        }
+
+    } // clickSignUp
+
+    private boolean checkspace() {
+
+        boolean result = true;
+        result = nameString.equals("") || userString.equals("") ||
+                passwordString.equals("") || IdCardString.equals("");
+        return result;
     }
-}
+
+
+    private void bindWidget() {
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        IdCardEditText = (EditText) findViewById(R.id.editText4);
+    }
+
+
+} //Main Class
